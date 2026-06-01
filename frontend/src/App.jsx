a48ff26bf3c7b8ba8e034ce59744a91438e30b97
HIMANSHU KUMAR <himanshukmourya7705@gmail.com>
2026-06-01 07:58:50 +0530

    bug fix

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Home from "./pages/Home";
@@ -19,12 +18,6 @@ function App() {
       </Routes>
     </BrowserRouter>
   );
-=======
-import AIInterviewer from "./AIInterviewer";
-
-function App() {
-  return <AIInterviewer />;
->>>>>>> 47f66c9bad8bb5b8db516fa0f72ef51645b09312
 }
 
 export default App;
